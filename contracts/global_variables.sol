b3cb717ffddb28855643202bd86abccd13fc0aa5
Moneshsoni <sonimonesh115@gmail.com>
2022-10-22 10:26:48 +0530

    add the number

--- contracts/global_variables.sol
+++ contracts/global_variables.sol
@@ -1,31 +1,33 @@
-pragma solidity  0.8.9;
-contract GlobalVariables{
-    function block_coinbase()public view returns(address){
+pragma solidity 0.8 .9;
+contract GlobalVariables {
+    function block_coinbase()public view returns(address) {
         return block.coinbase;
     }
-    
-    function block_difficulty()public view returns(uint){
+
+    function block_difficulty()public view returns(uint) {
         return block.difficulty;
-        
+
     }
     
-    function block_gaslimit()public view returns(uint){
+    
+
+    function block_gaslimit()public view returns(uint) {
         return block.gaslimit;
     }
-    
-    function block_number()public view returns(uint){
-        return  block.number;
+
+    function block_number()public view returns(uint) {
+        return block.number;
     }
-    
-    function block_timestamp()public view returns(uint){
+
+    function block_timestamp()public view returns(uint) {
         return block.timestamp;
     }
-    
-    function msg_data()public view returns(bytes calldata){
+
+    function msg_data()public view returns(bytes calldata) {
         return msg.data;
     }
-    
-    function msg_sender()public view returns(address){
+
+    function msg_sender()public view returns(address) {
         return msg.sender;
     }
 
@@ -33,11 +35,11 @@ contract GlobalVariables{
     //     return msg.value;
     // }
 
-    function tx_gasprice()public view returns(uint){
+    function tx_gasprice()public view returns(uint) {
         return tx.gasprice;
     }
 
-    function tx_origin()public view returns(address){
+    function tx_origin()public view returns(address) {
         return tx.origin;
     }
 }
