f57f391ec3250853ff8e837e15cb1f9731679561
Moneshsoni <sonimonesh115@gmail.com>
2022-10-24 00:06:26 +0530

    addd the extra ffunctionnality

--- contracts/simple_storage.sol
+++ contracts/simple_storage.sol
@@ -1,11 +1,21 @@
 pragma solidity 0.8.9;
 contract Storage{
     string public text;
+    string public text1;
+    //44601 gas cost
     function set_tex(string calldata _text)external{
         text = _text; 
     }
-    
-    function get_val()public view returns(string calldata){
+    //44973 gas cost
+    function set_text1(string memory _text1)external{
+        text1 = _text1; 
+    }
+
+    function get_text1()public view returns(string memory){
+        return text1;
+    }
+
+    function get_text()public view returns(string memory){
         return text;
     }
 }
