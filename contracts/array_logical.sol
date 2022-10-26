fb5362b0a005d2bdd8850988ce55e560f956cb66
Moneshsoni <sonimonesh115@gmail.com>
2022-10-26 22:07:02 +0530

    add library

---
+++ contracts/array_logical.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.8.9;
+library ArrayLib{
+    function find(uint[] storage arr, uint n)public view returns(uint){
+        for(uint i = 0; i<=arr.length;i++){
+            if(arr[i]==n){
+                return i;
+            }
+        }
+        revert("Can't found");
+    }
+}
+
+contract TestArray{
+    using ArrayLib for uint[];
+    uint[] public arr = [1,2,3];
+    function testFind() external view returns(uint i){
+        // return ArrayLib.find(arr,2);
+        return arr.find(2);
+    }
+}
