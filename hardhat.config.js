265b198d12441f95bab14fc6a84f700de7fa1856
Moneshsoni <sonimonesh115@gmail.com>
2022-11-03 09:52:36 +0530

    add the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity:
+  {
+    compilers:[
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.8.1",
+      },
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.8.9",
+      }
+    ]
+
+  },
+  networks:{
+    mumbai:{
+      url: process.env.ALCHEMY_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: 'T6ZRYP4Z6EM2QXUC8C4HCYIDWNSAAB4YZQ'
+    }
+  }
 };
