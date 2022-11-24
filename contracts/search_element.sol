c024546981ec6b4043727ea7d8d5337f548e37b1
Moneshsoni <sonimonesh115@gmail.com>
2022-11-25 00:11:41 +0530

    add the  element

---
+++ contracts/search_element.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.8.9;
+contract Array{
+    uint[] public arr;
+    uint public n=1;
+    function put_array(uint[] memory _arr)external{
+        arr = _arr;
+    }
+    
+    function check_element()public view returns(string memory){
+        for(uint i=0;i<arr.length;i++){
+            if(arr[i]==n){
+                return "number exiest";
+            }
+        }
+        return "number not exiest";
+    }
+}
