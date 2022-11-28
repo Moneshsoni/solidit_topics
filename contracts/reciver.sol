c11a75766c157e9ad75d869f3983b38d9d5a0f41
Moneshsoni <sonimonesh115@gmail.com>
2022-11-28 21:48:42 +0530

    add the delegatecall

--- contracts/reciver.sol
+++ contracts/reciver.sol
@@ -5,7 +5,7 @@ contract Receiver {
     event Received(address caller, uint amount, string message);
 
     fallback() external payable {
-        emit Received(msg.sender, msg.value, "Fallback was called");
+        emit Received(msg.sender, msg.value, "Fallback was called");`
     }
 
     function foo(string memory _message, uint _x) public payable returns (uint) {
